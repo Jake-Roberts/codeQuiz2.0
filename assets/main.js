70083206cab2254b7be3e49cc0d215836fe2eed5
JakeRoberts99 <jake.s.roberts99@gmail.com>
2022-01-07 00:30:12 -0700

    added basic file and added to the readme also created the object that houses my question

---
+++ assets/main.js
@@ -0,0 +1,22 @@
+const myQuestions = [
+    {
+        question: 'What is the greatest video game of all time?',
+        choice: ['Halo 3', 'The Witcher 3', 'Destiny 1', 'Skyrim'],
+        answer: 'Halo'
+    },
+    {
+        question: 'What is the best Movie of all time?',
+        choice: ['Bourne Identity', 'The Sandlot', 'Iron Man', 'Inception'],
+        answer: 'Iron Man'
+    },
+    {
+        question: 'Who is the best boxer of all time?',
+        choice: ['Muhammid Ali', 'Mike Tyson', 'Jake Paul LOL', 'Floyd Mayweather'],
+        answer: 'Muhammid Ali'
+    },
+    {
+        question: 'What is the best coding language of all time?',
+        choice: ['JavaScript', 'Python', 'C#', 'Go'],
+        answer: 'JavaScript'
+        },
+];
